644f991a4edbdd7010c4b75d8ca1683c2eb7c035
elinalivaprince <liiva.prince@gmail.com>
2026-02-02 13:27:44 +0200

    Update data.js 3

--- data.js
+++ data.js
@@ -9,7 +9,7 @@ import lovePic from './images/love.png';
 export const tweetsData = [   
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `trollPic`,
+        profilePic: trollPic,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -21,19 +21,19 @@ export const tweetsData = [
     },    
     {
         handle: `@Elon ✅`,
-        profilePic: `muskPic`,
+        profilePic: muskPic,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
                   {
                 handle: `@TomCruise ✅`,
-                profilePic: `tcruisePic`,
+                profilePic: tcruisePic,
                 tweetText: `Yes! Sign me up! 😎🛩`,
             },
                   {
                 handle: `@ChuckNorris ✅`,
-                profilePic: `chuckPic`,
+                profilePic: chuckPic,
                 tweetText: `I went last year😴`,
             },
         ],
@@ -43,19 +43,19 @@ export const tweetsData = [
     },
         {
         handle: `@NoobCoder12`,
-        profilePic: `flowerPic`,
+        profilePic: flowerPic,
         likes: 10,
         retweets: 3,
         tweetText: `Are you a coder if you only know HTML?`,
         replies: [
             {
                 handle: `@StackOverflower ☣️`,
-                profilePic: `overflowPic`,
+                profilePic: overflowPic,
                 tweetText: `No. Obviosuly not. Go get a job in McDonald's.`,
             },
             {
                 handle: `@YummyCoder64`,
-                profilePic: `lovePic`,
+                profilePic: lovePic,
                 tweetText: `You are wonderful just as you are! ❤️`,
             },
         ],
